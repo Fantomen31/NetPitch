56f1b9ee2e61f18ae0167209e52d39e1705f8d31
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-19 12:03:10 +0000

    added the profCreated a View to Handle Profile Updates

--- netpitch/views.py
+++ netpitch/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse 
+from .forms import ProfileForm
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profile.html', {'form': form})
 
 def my_netpitch(request):
     return HttpResponse("Hello NetPitch!")
