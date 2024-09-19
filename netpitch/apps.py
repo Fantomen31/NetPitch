caf1b6937f1c31066191900fd361c4a5c291fd85
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-19 12:01:58 +0000

    added ProfileForm to apps.py

--- netpitch/apps.py
+++ netpitch/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from .models import Profile
 
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_image', 'user_type']
 
 class NetpitchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
