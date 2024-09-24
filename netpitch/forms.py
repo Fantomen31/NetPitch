f5f6a7d38ec7c5d42585fa59f2d821bf33a7fbee
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-24 08:13:32 +0000

    extended the django usercreation form to incoroprate producer or writer profiles

--- netpitch/forms.py
+++ netpitch/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+
+class CustomUserCreationForm(UserCreationForm):
+    USER_TYPE_CHOICES = [
+        ('Writer', 'Writer'),
+        ('Producer', 'Producer'),
+    ]
+
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'user_type']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            profile = Profile.objects.create(user=user, user_type=self.cleaned_data['user_type'])
+            profile.save()
+        return user
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
