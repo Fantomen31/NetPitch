a489c2e31e6e6d6eeb9266391a65a0cc41191e3a
The_phantom walker <Neocoinkitt@gmail.com>
2024-09-24 09:42:53 +0000

    created genre, pitchdeck and Collaboration models for future sections.

--- netpitch/models.py
+++ netpitch/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Profile(models.Model):
     USER_TYPE_CHOICES = [
         ('Writer', 'Writer'),
-        ('producer', 'Producer'),
+        ('Producer', 'Producer'),
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,3 +20,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class PitchDeck(models.Model):
+    PITCH_TYPE_CHOICES = [
+        ('Film', 'Film'),
+        ('TV Show', 'TV Show'),
+    ]
+
+    title = models.CharField(max_length=255)
+    synopsis = models.TextField()
+    genre = models.ForeignKey('netpitch.Genre', on_delete=models.SET_NULL, null=True)  # String reference for Genre
+    pitch_type = models.CharField(max_length=10, choices=PITCH_TYPE_CHOICES)
+    writer = models.ForeignKey('netpitch.Profile', on_delete=models.CASCADE)  # String reference for Profile
+
+    def __str__(self):
+        return self.title
+
+class CollaborationRequest(models.Model):
+    pitch = models.ForeignKey('netpitch.PitchDeck', on_delete=models.CASCADE)  # Use string reference for PitchDeck
+    producer = models.ForeignKey('netpitch.Profile', on_delete=models.CASCADE)  # Use string reference for Profile
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Collaboration request for {self.pitch.title} by {self.producer.user.username}"
